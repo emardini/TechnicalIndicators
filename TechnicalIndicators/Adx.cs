79f409156c8b88d06035ed371a912abb1590ca4d
Eduardo Mardini <emardini@outsitenetworks.com>
2015-07-23 00:21:22 -0400

    Add dpocumentation for adx calculation

--- TechnicalIndicators/Adx.cs
+++ TechnicalIndicators/Adx.cs
@@ -2,6 +2,8 @@
 {
     using System.Collections.Generic;
 
+    //Use http://www.dinosaurtech.com/2007/average-directional-movement-index-adx-formula-in-c-2/
+    //Or http://www.theforexguy.com/average-directional-index-indicator/
     public class Adx
     {
         public IEnumerable<decimal> Values { get; set; }
